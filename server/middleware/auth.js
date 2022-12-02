9342c33ad66ce8c5b8995a45206e6b54d08bee95
mcpeck <peckmelodie@gmail.com>
2022-12-01 22:29:31 -0600

    Share progress with pair

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,59 +3,38 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   //access parsed cookies on the request
-  // req.cookies = cookiesObj;
   var parsedCookies = req.cookies;
-  // console.log('parsedCookies ',parsedCookies);
-  //look up user data related to that session
-  if(Object.keys(parsedCookies).length === 0) {
-    models.Sessions.create()
-    .then(result => {
-      return models.Sessions.get({id : result.insertId})
-    })
-    .then(result => {
-      req.session = {hash: result.hash}
-      res.cookie('shortlyid', result.hash);
-      next();
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  } else { // if an incoming request has a cookie, verify that the cookie is valid (that is a session stored in the db)
-    // console.log('parsedCookies', parsedCookies)
-    var session;
-    models.Sessions.get({hash: parsedCookies.shortlyid})
-    .then(result => {
-
-      session = result;
-      console.log('session get result',result);
-      // console.log('session userId',result.userId);
-      if(result.userId) {
-        return models.Users.get({id: result.userId});
-      } else {
-        req.session = {'hash': parsedCookies.shortlyid};
+  // verify that the cookie is valid (that is a session stored in the db)
+  models.Sessions.get({hash: parsedCookies.shortlyid})
+  .then(result => {
+    if (result) {
+      // if the cookie is valid, get the associated user info
+      models.Users.get({id: result.userId})
+      .then(result => {
+        if(result) {
+          var username = Object.values(JSON.parse(JSON.stringify(result)))[1];
+          var userId = Object.values(JSON.parse(JSON.stringify(result)))[0];
+          req.session = {'hash': parsedCookies.shortlyid, 'user': {'username': username}, 'userId': userId}
+          next();
+        } else {
+          req.session = {'hash': parsedCookies.shortlyid, 'user': {'username': ''}, 'userId': ''};
+          next();
+        }
+      })
+    } else {
+      // if the cookie is not valid, generate a new hash and session object
+      models.Sessions.create()
+      .then(result => {
+        return models.Sessions.get({id : result.insertId})
+      })
+      .then(result => {
+        req.session = {hash: result.hash}
+        res.cookie('shortlyid', result.hash);
         next();
-      }
-    })
-    .then(result => {
-      var username;
-      var userId = '';
-      if(!result) {
-        username = null;
-      } else {
-        username = Object.values(JSON.parse(JSON.stringify(result)))[1];
-        userId = Object.values(JSON.parse(JSON.stringify(result)))[0];
-      }
-      req.session = {'hash': parsedCookies.shortlyid, 'user': {'username': username}, 'userId': userId}
-      next();
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }
-  //assigns an object to to a session property on the request that contains relevant user info
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }})
 };
-
-/************************************************************/
-// Add additional authentication middleware functions below
-/************************************************************/
 
